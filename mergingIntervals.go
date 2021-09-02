e8b3bcf135cdcbab30a2ec9e85cb00ca048d50ac
UgurTheG <49240783+UgurTheG@users.noreply.github.com>
2021-09-02 21:55:19 +0200

    Added an interval object, added consts to eliminate redundancy in code, added comments

--- mergingIntervals.go
+++ mergingIntervals.go
@@ -1,33 +1,61 @@
 package mergingIntervals
 
-import (
-	"sort"
+import "sort"
+
+const (
+	// FirstIntervalElement is used to index the first interval in the slice of intervals.
+	FirstIntervalElement = 0
+	// StartOfInterval is used to index the startpoint of an interval.
+	StartOfInterval = 0
+	// EndOfInterval is used to index the endpoint of an interval.
+	EndOfInterval = 1
+	// IntervalLengthCheck is used to check the length of an interval-slice.
+	IntervalLengthCheck = 2
 )
 
-func Merge(intervalSlice [][]int) [][]int {
-	if len(intervalSlice) < 2 {
+// Interval provides functions to merge and sort a slice of intervals.
+type Interval struct{}
+
+// Merge invokes helper-functions in order to sort and return a slice of merged intervals.
+func (i Interval) Merge(intervalSlice [][]int) [][]int {
+
+	// If there are less than two Intervals in the slice of Intervals we can return the slice of intervals without doing anything.
+	if len(intervalSlice) < IntervalLengthCheck {
 		return intervalSlice
 	}
-	// sort by startpoint of interval
-	sort.Slice(intervalSlice, func(i, j int) bool {
-		return intervalSlice[i][0] < intervalSlice[j][0]
-	})
+	i.sortIntervalsByStart(intervalSlice)
+	return i.mergeIntervals(intervalSlice)
+
+}
+
+// mergeIntervals returns a slice of merged intervals.
+func (i Interval) mergeIntervals(intervals [][]int) [][]int {
 
 	returnedIntervals := [][]int{}
+	// the start and endpoint of the first interval in the slice is used as a comparison for the following intervals.
+	tmpIntervalStart := intervals[FirstIntervalElement][StartOfInterval]
+	tmpIntervalEnd := intervals[FirstIntervalElement][EndOfInterval]
 
-	tmpIntervalStart := intervalSlice[0][0]
-	tmpIntervalEnd := intervalSlice[0][1]
-
-	for _, interval := range intervalSlice[0:] {
-		if tmpIntervalEnd >= interval[0] && tmpIntervalEnd < interval[1] {
-			tmpIntervalEnd = interval[1]
-		} else if tmpIntervalEnd < interval[0] {
+	for _, intervalElement := range intervals {
+		// if: If the current interval can be merged with the previous interval, the endpoint of the previous interval is updated with the endpoint of the current interval.
+		if tmpIntervalEnd >= intervalElement[StartOfInterval] && tmpIntervalEnd < intervalElement[EndOfInterval] {
+			tmpIntervalEnd = intervalElement[EndOfInterval]
+			// elseif: If the current interval is unmergeable with the previous interval, append it to the returnedIntervals slice.
+			// Next, the start and endpoint of the current interval is used for comparison as tmpIntervalStart and tmpIntervalEnd.
+		} else if tmpIntervalEnd < intervalElement[StartOfInterval] {
 			returnedIntervals = append(returnedIntervals, []int{tmpIntervalStart, tmpIntervalEnd})
-			tmpIntervalStart = interval[0]
-			tmpIntervalEnd = interval[1]
+			tmpIntervalStart = intervalElement[StartOfInterval]
+			tmpIntervalEnd = intervalElement[EndOfInterval]
 		}
 	}
-	returnedIntervals = append(returnedIntervals, []int{tmpIntervalStart, tmpIntervalEnd})
 
+	returnedIntervals = append(returnedIntervals, []int{tmpIntervalStart, tmpIntervalEnd})
 	return returnedIntervals
+}
+
+// sortIntervalsByStart sorts the slice of intervals by its startpoints.
+func (i Interval) sortIntervalsByStart(intervals [][]int) {
+	sort.Slice(intervals, func(firstIntervalIndex, secondIntervalIndex int) bool {
+		return intervals[firstIntervalIndex][StartOfInterval] < intervals[secondIntervalIndex][StartOfInterval]
+	})
 }
